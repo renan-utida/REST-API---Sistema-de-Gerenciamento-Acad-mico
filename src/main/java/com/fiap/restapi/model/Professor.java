75b2a7391055223fd5913d343d14211994176801
Renan <renandiutida@gmail.com>
2025-10-03 14:03:45 -0300

    model/Professor.java: Iniciando desafio de cadastro de professores, criando a classe Professor, com atributos id, nome, departamento, email e titulacao

---
+++ src/main/java/com/fiap/restapi/model/Professor.java
@@ -0,0 +1,16 @@
+package com.fiap.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//Data: possibilita que a pessoa não precise informar getters/setters
+//AllArgsConstructor: possibilita que a pessoa não precise informar o construtor
+@Data
+@AllArgsConstructor
+public class Professor {
+    private Long id;
+    private String nome;
+    private String departamento;
+    private String email;
+    private String titulacao;
+}
